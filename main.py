90650714f0c91d6d08550efa4d2b44c6f4bd3039
Zncl2222 <3002shinning@gmail.com>
2021-12-30 17:12:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import sys
 import tkinter as tk
 import os
+import numpy as np
+import pandas as pd
+import time
+import ttkthemes as th
+import tkinter as tk
+import tkinter.ttk as ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+from tkinter import colorchooser
+import pickle
+from multiprocessing import Pool
 
 os.chdir(os.path.dirname(__file__))
 
@@ -20,7 +34,8 @@ from gui import main_GUI
 
 if __name__ == '__main__':             
     root = tk.Tk()
-    
+
     Window=main_GUI(root)
+    #Window=Options_GUI()
 
     root.mainloop()
